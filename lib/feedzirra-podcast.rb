16a41654734b171175c90003309d509b7085c63f
Chris Kalafarski <chris@farski.com>
2013-07-26 13:35:47 -0400

    Cleanup

---
+++ lib/feedzirra-podcast.rb
@@ -0,0 +1,11 @@
+require 'periodic'
+require 'feedzirra'
+
+require 'feedzirra-podcast/parser/rss2'
+require 'feedzirra-podcast/parser/rss2_channel_image'
+require 'feedzirra-podcast/parser/rss2_item'
+
+require 'feedzirra-podcast/parser/podcast'
+require 'feedzirra-podcast/parser/podcast_item'
+
+require 'feedzirra-podcast/parser/itunes_owner'
