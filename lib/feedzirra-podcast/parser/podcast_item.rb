eb6c47413036c971976d0df84b9ef442d06d9829
Chris Kalafarski <chris@farski.com>
2013-07-29 16:19:35 -0400

    Include some DC elements

--- lib/feedzirra-podcast/parser/podcast_item.rb
+++ lib/feedzirra-podcast/parser/podcast_item.rb
@@ -18,6 +18,15 @@ module FeedzirraPodcast
       element :"itunes:itunes:isClosedCaptioned", as: :itunes_is_closed_captioned_string
       element :"itunes:order", as: :itunes_order_string
 
+      # DC
+
+      element :"dc:creator", as: :dc_creator
+      element :"dc:created", as: :dc_created_string
+
+      # Content
+
+      element :"content:encoded", as: :content_encoded
+
       def itunes_explicit
         case itunes_explicit_string
         when "yes"
@@ -59,6 +68,12 @@ module FeedzirraPodcast
 
       def itunes_order
         itunes_order_string.to_f if itunes_order_string.present?
+      end
+
+      def dc_created
+        Time.parse(dc_created_string).utc if dc_created_string.present?
+      rescue ArgumentError
+        nil
       end
     end
   end
