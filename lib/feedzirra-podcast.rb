4b6df167af84bd9030fc54d6e4050cf2b96bb0f2
Chris Kalafarski <chris@farski.com>
2014-02-10 16:46:35 -0500

    Backwards compatibility

--- lib/feedzirra-podcast.rb
+++ lib/feedzirra-podcast.rb
@@ -9,3 +9,13 @@ require 'feedzirra-podcast/parser/podcast'
 require 'feedzirra-podcast/parser/podcast_item'
 
 require 'feedzirra-podcast/parser/itunes_owner'
+
+class Object
+  def present?
+    !blank?
+  end
+
+  def blank?
+    respond_to?(:empty?) ? empty? : !self
+  end
+end
